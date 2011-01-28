da040bc5a6696cb90e532584e0b129da888af359
steflewandowski <stef@stef.io>
2011-01-28 14:23:14 +0000

    Overriding Backbone.sync for all models is okay in some cases. This change allows you to choose which models you want to use localStorage with by setting MyBackBoneModel.sync = Backbone.localSync on a per model basis. This is in line with a recent change to Backbone to allow per-model sync overrides. To use globally just do Backbone.sync = Backbone.localSync

--- backbone.localStorage.js
+++ backbone.localStorage.js
@@ -64,9 +64,10 @@ _.extend(Store.prototype, {
 
 });
 
-// Override `Backbone.sync` to use delegate to the model or collection's
+// Create a `Backbone.localSync` to use delegate to the model or collection's
+// We can then set MyBackBoneModel.sync = Backbone.localSync to use local storage with that model
 // *localStorage* property, which should be an instance of `Store`.
-Backbone.sync = function(method, model, options) {
+Backbone.localSync = function(method, model, options) {
 
   var resp;
   var store = model.localStorage || model.collection.localStorage;
