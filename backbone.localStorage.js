8fccd62a0949cbc815efd976acdc3f77948e9731
Jerome Gravel-Niquet <jeromegn@gmail.com>
2011-08-14 10:54:37 -0300

    Added version number

--- backbone.localStorage.js
+++ backbone.localStorage.js
@@ -1,3 +1,10 @@
+/**
+ * Backbone localStorage Adapter v1.0
+ * https://github.com/jeromegn/Backbone.localStorage
+ *
+ * Date: Sun Aug 14 2011 09:53:55 -0400
+ */
+
 // A simple module to replace `Backbone.sync` with *localStorage*-based
 // persistence. Models are given GUIDS, and saved into a JSON object. Simple
 // as that.
