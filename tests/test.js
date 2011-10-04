94c9cfa9ea27c4b34707d55597f43025481d876c
Martin Häcker <spamfaenger@gmx.de>
2011-10-04 19:26:53 +0200

    Split tests into smaller tests to make it easier to document what they actually test.

--- tests/test.js
+++ tests/test.js
@@ -1,13 +1,9 @@
 $(document).ready(function() {
-    module("localStorage");
-
     var Library = Backbone.Collection.extend({
         localStorage: new window.Store("libraryStore")
         
         // is the problem with my library that is has no model reference?
     });
-
-    var library = new Library();
 
     var attrs = {
         title  : 'The Tempest',
@@ -15,42 +11,48 @@ $(document).ready(function() {
         length : 123
     };
     
-    // Make sure there is no library collection when we start
+    var library = null;
     
-    var killStorage = function (name) {
-        for (var i = 0; i < localStorage.length; i++) {
-            var key = localStorage.key(i);
-            if (key.indexOf(name) !== -1) {
-                localStorage.removeItem(key);
-            }
+    module("localStorage", {
+        setup: function() {
+            localStorage.clear();
+            library = new Library();
         }
-            
-    };
-
-    // read from the library object that shouldn't exist when we start
-    test("collection", function() {
-        killStorage('libraryStore');
+    });
+    
+    test("should be empty initially", function() {
+        equals(library.length, 0, 'empty initially');
         library.fetch();
         equals(library.length, 0, 'empty read');
-        
+    });
+    
+    test("should create item", function() {
         library.create(attrs);
         equals(library.length, 1, 'one item added');
         equals(library.first().get('title'), 'The Tempest', 'title was read');
         equals(library.first().get('author'), 'Bill Shakespeare', 'author was read');
         equals(library.first().get('length'), 123, 'length was read');
-
+    });
+    
+    test("should allow to change id", function() {
+        library.create(attrs);
         library.first().save({id: '1-the-tempest', author: 'William Shakespeare'});
         equals(library.first().get('id'), '1-the-tempest', 'verify ID update');
         equals(library.first().get('title'), 'The Tempest', 'verify title is still there');
         equals(library.first().get('author'), 'William Shakespeare', 'verify author update');
         equals(library.first().get('length'), 123, 'verify length is still there');
+    });
+    
+    test("should remove from collection", function() {
+        library.create(attrs);
         library.each(function(book) {
             book.destroy();
         });
         equals(library.length, 0, 'item was destroyed and library is empty');
+    });
+    
         
     });
-
 /*
 
   test("sync: update", function() {
