43d5fc57c401752865cee7c4302752f1597afb3c
Robert Hall <robert@robthehall.com>
2011-10-27 21:27:48 -0700

    * Compile code with Google's Closure Compiler * Whitespace and style changes: update comments, rename some variables, clean up code

--- backbone.localStorage.js
+++ backbone.localStorage.js
@@ -12,59 +12,70 @@
 // Generate four random hex digits.
 function S4() {
    return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-};
+}
 
 // Generate a pseudo-GUID by concatenating random hexadecimal.
 function guid() {
    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
-};
+}
 
-// Our Store is represented by a single JS object in *localStorage*. Create it
+// Our Store is represented by a list of model IDs, and the stringified
+// version of the models, in *localStorage*. Create it
 // with a meaningful name, like the name you'd give a table.
 window.Store = function(name) {
   this.name = name;
   var store = localStorage.getItem(this.name);
-  this.records = (store && store.split(",")) || [];
+  this.ids = (store && store.split(",")) || [];
 };
 
 _.extend(Store.prototype, {
 
-  // Save the current state of the **Store** to *localStorage*.
+  // Save the IDs of the models to *localStorage*.
+  // (Call Store.update(model) to save the model to localStorage.)
   save: function() {
-    localStorage.setItem(this.name, this.records.join(","));
+    localStorage.setItem(this.name, this.ids.join(","));
   },
 
   // Add a model, giving it a (hopefully)-unique GUID, if it doesn't already
   // have an id of it's own.
   create: function(model) {
-    if (!model.id) model.id = model.attributes.id = guid();
+    if (!model.id) {
+      model.id = model.attributes.id = guid();
+    }
     localStorage.setItem(this.name+"-"+model.id, JSON.stringify(model));
-    this.records.push(model.id.toString());
+    this.ids.push(model.id.toString());
     this.save();
     return model;
   },
 
-  // Update a model by replacing its copy in `this.data`.
+  // Update a model by replacing its copy in *localStorage*.
   update: function(model) {
     localStorage.setItem(this.name+"-"+model.id, JSON.stringify(model));
-    if (!_.include(this.records, model.id.toString())) this.records.push(model.id.toString()); this.save();
+    if (!_.include(this.ids, model.id.toString())) {
+      this.ids.push(model.id.toString());
+    }
+    this.save();
     return model;
   },
 
-  // Retrieve a model from `this.data` by id.
+  // Retrieve a model from *localStorage* by id.
   find: function(model) {
     return JSON.parse(localStorage.getItem(this.name+"-"+model.id));
   },
 
   // Return the array of all models currently in storage.
   findAll: function() {
-    return _.map(this.records, function(id){return JSON.parse(localStorage.getItem(this.name+"-"+id));}, this);
+    return _.map(this.ids, function(id) {
+      return JSON.parse(localStorage.getItem(this.name+"-"+id));
+    }, this);
   },
 
-  // Delete a model from `this.data`, returning it.
+  // Delete a model from *localStorage*, returning it.
   destroy: function(model) {
     localStorage.removeItem(this.name+"-"+model.id);
-    this.records = _.reject(this.records, function(record_id){return record_id == model.id.toString();});
+    this.ids = _.reject(this.ids, function(id) {
+      return id == model.id.toString();
+    });
     this.save();
     return model;
   }
