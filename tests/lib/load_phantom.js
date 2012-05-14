ccac5f1151aa5e47676ea7d4eca8b2e2c74df516
Juho Vähä-Herttua <juhovh@iki.fi>
2012-05-14 19:05:10 +0300

    Update load_phantom.js to run the new tests on Travis CI

--- tests/lib/load_phantom.js
+++ tests/lib/load_phantom.js
@@ -1,26 +1,32 @@
 var fs = require('fs')
-  , page = new WebPage()
-  , file = fs.absolute('tests/test.html');
+  , pages = [ new WebPage(), new WebPage() ]
+  , files = [ fs.absolute('tests/test.html'), fs.absolute('tests/test2.html') ]
+  , i, page, file;
 
-page.onConsoleMessage = function(msg) {
-  console.log(msg);
-  if (msg === "success")
-    phantom.exit(0);
-  else
+for (i in pages) {
+  page = pages[i];
+  file = files[i];
+
+  page.onConsoleMessage = function(msg) {
+    console.log(msg);
+    if (msg === "success")
+      phantom.exit(0);
+    else
+      phantom.exit(1);
+  };
+
+  page.onError = function (msg, trace) {
+    console.log(msg);
+    trace.forEach(function(item) {
+      console.log('  ', item.file, ':', item.line);
+    });
     phantom.exit(1);
-};
+  };
 
-page.onError = function (msg, trace) {
-  console.log(msg);
-  trace.forEach(function(item) {
-    console.log('  ', item.file, ':', item.line);
+  page.open('file://' + file, function (status) {
+    if (status !== 'success') {
+      console.log('Failed to load the address');
+      return phantom.exit(1);
+    }
   });
-  phantom.exit(1);
-};
-
-page.open('file://' + file, function (status) {
-  if (status !== 'success') {
-    console.log('Failed to load the address');
-    return phantom.exit(1);
-  }
-});
+}
