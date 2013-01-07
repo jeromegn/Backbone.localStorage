d4f07c85122eaee3d47b1cdefe74da2b4c42b80a
Nils Ekman <nisse.ekman@gmail.com>
2013-01-07 22:50:40 +0100

    Use global variables if local variables are undefined.

--- backbone.localStorage.js
+++ backbone.localStorage.js
@@ -3,15 +3,18 @@
  *
  * https://github.com/jeromegn/Backbone.localStorage
  */
-(function (factory) {
+(function (root, factory) {
    if (typeof define === "function" && define.amd) {
       // AMD. Register as an anonymous module.
-      define("backbone.localstorage",["underscore","backbone"], factory);
+      define(["underscore","backbone"], function(_, Backbone) {
+        // Use global variables if the locals is undefined.
+        return factory(_ || root._, Backbone || root.Backbone);
+      });
    } else {
       // RequireJS isn't being used. Assume underscore and backbone is loaded in <script> tags
       factory(_, Backbone);
    }
-}(function(_, Backbone) {
+}(this, function(_, Backbone) {
 // A simple module to replace `Backbone.sync` with *localStorage*-based
 // persistence. Models are given GUIDS, and saved into a JSON object. Simple
 // as that.
